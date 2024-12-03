bf68c3aedbbea14256542df8e9721cbc65529cb0
faisal-foyez <faisalislam64@gmail.com>
2024-12-03 10:46:31 +0600

    Add some utility funciton
    - Include clsx package cn function to manage the class names with coditional logic.
    - Separate the withThemeDecorator for storybook stories

---
+++ packages/shared_ui/src/utils/storybook/withThemeDecorator.tsx
@@ -0,0 +1,14 @@
+export const withThemeDecorator = (Story: any) => {
+  return (
+    <div style={{ display: 'flex', gap: '30px' }}>
+      <div style={{ padding: '20px', background: '#ffffff' }}>
+        <h3 style={{textAlign: 'center'}}>Light Mode</h3>
+        <Story />
+      </div>
+      <div data-theme='dark' style={{ padding: '20px', borderRadius: '10px', background: '#1C222B', color: '#ffffff' }}>
+        <h3 style={{ color: '#ffffff', textAlign: 'center' }}>Dark Mode</h3>
+        <Story />
+      </div>
+    </div>
+  );
+};
