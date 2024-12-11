e844b7413ccbe9ef4bad6ee431d26ab6ab53635e
faisal-foyez <faisalislam64@gmail.com>
2024-12-11 08:09:07 +0600

    Update notification desgin and storybook
    - Include Notfication dark theme design in Notification.scss
    - Include dark designs in Notification storybook

--- packages/shared_ui/src/Molecules/Notification/stories/Notification.stories.tsx
+++ packages/shared_ui/src/Molecules/Notification/stories/Notification.stories.tsx
@@ -51,6 +51,24 @@ export const Default: Story = {
   )
 };
 
+export const DarkDefault: Story = {
+  args: {
+    position: 'CENTER',
+    isShowClose: true,
+    dismiss: false
+  },
+  render: (args) => (
+    <div data-theme="dark" style={{height: '30vh'}}>
+      <Notification {...args}>
+        <NotificationContent>
+          <NotificationTitle>Can we store cookies?</NotificationTitle>
+          <NotificationDescription>We use cookies to ensure you get the best experience on our website. By using our website, you agree to our use of cookies. Learn more.</NotificationDescription>
+        </NotificationContent>
+      </Notification>
+
+    </div>
+  )
+};
 
 export const NotificationWithImageTopAndButtons: Story = {
   args: {
@@ -78,6 +96,32 @@ export const NotificationWithImageTopAndButtons: Story = {
   )
 };
 
+export const DarkNotificationWithImageTopAndButtons: Story = {
+  args: {
+    position: 'CENTER',
+    isShowClose: true,
+    dismiss: false
+  },
+  render: (args) => (
+    <div data-theme="dark" style={{height: '40vh'}}>
+      <Notification {...args}>
+        <div style={{display: 'flex', flexDirection: 'column', gap: '10px'}}>
+          <img style={{width: '100%', height: '200px', objectFit: 'cover'}} src={PicOne} />
+          <NotificationContent>
+            <NotificationTitle>Can we store cookies?</NotificationTitle>
+            <NotificationDescription>We use cookies to ensure you get the best experience on our website. By using our website, you agree to our use of cookies. Learn more.</NotificationDescription>
+          </NotificationContent>
+
+        </div>
+        <div style={{display: 'flex', flexDirection: 'row', gap: '10px', alignSelf:'flex-end'}}>
+          <Button type='primary' size='sm'>Accept</Button>
+          <Button type='error' size='sm'>Reject</Button>
+        </div>
+      </Notification>
+    </div>
+  )
+};
+
 export const NotificationWithImageLeftAndButtons: Story = {
   args: {
     position: 'CENTER',
@@ -86,6 +130,31 @@ export const NotificationWithImageLeftAndButtons: Story = {
   },
   render: (args) => (
     <div style={{height: '30vh'}}>
+      <Notification {...args}>
+        <div style={{display: 'flex', flexDirection: 'row', gap: '10px'}}>
+          <img style={{width: '50px', height: '50px', objectFit: 'cover'}} src={PicOne} />
+          <NotificationContent>
+            <NotificationTitle>Can we store cookies?</NotificationTitle>
+            <NotificationDescription>We use cookies to ensure you get the best experience on our website. By using our website, you agree to our use of cookies. Learn more.</NotificationDescription>
+          </NotificationContent>
+        </div>
+        <div style={{display: 'flex', flexDirection: 'row', gap: '10px', alignSelf:'flex-end'}}>
+          <Button type='primary' size='sm'>Accept</Button>
+          <Button type='error' size='sm'>Reject</Button>
+        </div>
+      </Notification>
+    </div>
+  )
+};
+
+export const DarkNotificationWithImageLeftAndButtons: Story = {
+  args: {
+    position: 'CENTER',
+    isShowClose: true,
+    dismiss: false
+  },
+  render: (args) => (
+    <div data-theme="dark" style={{height: '30vh'}}>
       <Notification {...args}>
         <div style={{display: 'flex', flexDirection: 'row', gap: '10px'}}>
           <img style={{width: '50px', height: '50px', objectFit: 'cover'}} src={PicOne} />
